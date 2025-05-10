81076028752cd569249fb5cf0204897b62fdd7c2
Peppe5G <p.stroberg@gmail.com>
2025-05-10 21:25:24 +0200

    Create slproxy.js

---
+++ api/slproxy.js
@@ -0,0 +1,15 @@
+export default async function handler(req, res) {
+  const apiKey = '39c9ef05880a432ab30b7ced4d6a9764'; 
+  const siteId = '1724'; // Safirgränd
+  const timeWindow = '30';
+
+  const url = `https://api.sl.se/api2/realtimedeparturesV4.json?key=${apiKey}&siteid=${siteId}&timewindow=${timeWindow}`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: 'Något gick fel vid hämtning av data.' });
+  }
+}
