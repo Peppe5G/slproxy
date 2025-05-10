8a1f6c2dc5643d6dc2bdcaf7bf42b836425aa3cb
Peppe5G <p.stroberg@gmail.com>
2025-05-10 21:44:48 +0200

    Update departures.js
    Fix: lade in SL-nyckel i proxy

--- api/departures.js
+++ api/departures.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
-  const apiKey = '39c9ef05880a432ab30b7ced4d6a9764'; 
-  const siteId = '1724'; // Safirgränd
-  const timeWindow = '30';
+  const apiKey = "39c9ef05880a432ab30b7ced4d6a9764"; // Din personliga SL-nyckel
+  const siteId = "1724"; // Safirgränd
+  const timeWindow = "30";
 
   const url = `https://api.sl.se/api2/realtimedeparturesV4.json?key=${apiKey}&siteid=${siteId}&timewindow=${timeWindow}`;
 
@@ -10,6 +10,6 @@ export default async function handler(req, res) {
     const data = await response.json();
     res.status(200).json(data);
   } catch (error) {
-    res.status(500).json({ error: 'Något gick fel vid hämtning av data.' });
+    res.status(500).json({ error: "Något gick fel vid hämtning av data." });
   }
 }
