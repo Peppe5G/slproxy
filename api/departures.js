d5e9e886b0ec22a17ef2218c7f3dcfa91b8a74e7
Peppe5G <p.stroberg@gmail.com>
2025-05-10 21:57:19 +0200

    Update departures.js

--- api/departures.js
+++ api/departures.js
@@ -1,17 +1,14 @@
-import fetch from 'node-fetch';
-
 export default async function handler(req, res) {
-  const apiKey = "39c9ef05880a432ab30b7ced4d6a9764";
-  const siteId = "1724"; // Safirgränd
-  const timeWindow = "30";
-
-  const url = `https://api.sl.se/api2/realtimedeparturesV4.json?key=${apiKey}&siteid=${siteId}&timewindow=${timeWindow}`;
+  const url = "https://transport.integration.sl.se/v1/sites/9302/departures";
 
   try {
     const response = await fetch(url);
     const data = await response.json();
     res.status(200).json(data);
   } catch (error) {
-    res.status(500).json({ error: "Något gick fel vid hämtning av data.", details: error.message });
+    res.status(500).json({
+      error: "Kunde inte hämta data från SL:s öppna API.",
+      details: error.message,
+    });
   }
 }
