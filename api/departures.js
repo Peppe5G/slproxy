00b3367ece07ac64b1d84cedb585ae0e19eaf714
Peppe5G <p.stroberg@gmail.com>
2025-05-10 22:30:52 +0200

    Update departures.js

--- api/departures.js
+++ api/departures.js
@@ -1,4 +1,6 @@
 export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
   const url = "https://transport.integration.sl.se/v1/sites/9302/departures";
 
   try {
